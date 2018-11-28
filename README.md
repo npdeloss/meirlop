5df7ff643a4b324f2c9f00f4e8e490f532bf6237
Nathaniel Delos Santos <npdeloss@ucsd.edu>
2018-11-28 13:29:20 -0800

    Added usage notebook and setup.py

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Motif Enrichment In Ranked Lists Of Peaks
 This project analyzes the relative enrichment of transcription factor binding motifs found in peaks at the top or bottom of a given ranking/score. The design is based on [MOODS](https://github.com/jhkorhonen/MOODS/tree/master/python) and [GSEApy](https://github.com/zqfang/GSEApy).
+
+# Usage
+Full usage example available through a [jupyter notebook](notebooks/usage.ipynb)
+
+```
+start = timer()
+print(datetime.datetime.now())
+
+from meirlop import analyze_peaks_with_prerank
+from tqdm import tqdm_notebook
+
+n_jobs = 20
+rs = np.random.RandomState(1234)
+
+analysis_results = analyze_peaks_with_prerank(peak_score_df = score_df,
+                                              peak_set_dict = motif_peak_set_dict,
+                                              peak_strata_df = strata_df,
+                                              min_set_size = 1,
+                                              max_set_size = np.inf,
+                                              nperm = 10,
+                                              nshuf = 100,
+                                              rs = rs,
+                                              n_jobs = n_jobs,
+                                              progress_wrapper = tqdm_notebook)
+
+enrichment_score_results_df, shuffled_permuted_peak_data, peak_idx_to_peak_id = analysis_results
+
+end = timer()
+runtime = end - start
+print(f'{runtime} seconds')
+print(datetime.datetime.now())
+```
---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import sys, os
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+
+__version__='0.1.1'
+
+# if sys.argv[-1] == 'publish':
+#     os.system("python setup.py sdist bdist_wheel register upload")
+#     print("  git tag -a %s -m 'version %s'" % (__version__,__version__))
+#     print("  git push --tags")
+#     sys.exit()
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='meirlop',
+      version=__version__,
+      description='Motif Enrichment In Ranked Lists Of Peaks',
+      long_description=('This project analyzes the relative enrichment of '
+      'transcription factor binding motifs found in peaks at the top or bottom of '
+      'a given ranking/score. The design is based on MOODS and GSEApy.'),
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX',
+          'Topic :: Software Development :: Libraries'
+          'Topic :: Scientific/Engineering :: Bio-Informatics'],
+      keywords= ['Motif', 'Enrichment',
+          'Bioinformatics'],
+      url='https://github.com/npdeloss/meirlop',
+      author='Nathaniel Delos Santos',
+      author_email='npdeloss@ucsd.edu',
+      license='MIT',
+      packages=['meirlop'],
+      package_data={'meirlop': ['notebooks/*.ipynb', 'notebooks/*.txt'],},
+      include_package_data=True,
+      install_requires=[
+                        'biopython>=1.72',
+                        'joblib>=0.12',
+                        'numpy>=1.13',
+                        'pandas>=0.23',
+                        'pybedtools>=0.7',
+                        'scikit-learn>=0.19',
+                        'statsmodels>=0.9',
+                        'tqdm>=4.28'
+                        ],
+      zip_safe=False,
+      download_url='https://github.com/npdeloss/meirlop',)
+
+__author__ = 'Nathaniel Delos Santos'
